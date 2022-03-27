d3e04a0c82790051caf9a2abc57367e4cb1b4b2a
Joaquim Marques <91548193+Joaquim1302@users.noreply.github.com>
2022-03-27 09:09:59 -0300

    Update 5_3digadj.py

--- src/lista02/5_3digadj.py
+++ src/lista02/5_3digadj.py
@@ -1,8 +1,12 @@
-n = 1234556
+n = 191234536
 
 i = n
-while i > 0:
-    i %= 100
-    j = i // 10
+while i // 10 != 0:
+    j = (i % 100) // 10
     k = i % 10
-    a = 0
+    if j == k:
+        print("a resposta é sim")
+        break
+    i //= 10
+if j != k:
+    print("a resposta é não")
