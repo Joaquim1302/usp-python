3865a12255cd4c40f83241a38f582ed5f818896f
Joaquim Marques <91548193+Joaquim1302@users.noreply.github.com>
2022-03-31 10:33:21 -0300

    Up Coh Piah

--- src/coh_piah.py
+++ src/coh_piah.py
@@ -1,19 +1,58 @@
 import re
 
 
+# def input_int(msg, minimo, maximo, valor_default, msg_err):
+#     while True:
+#         try:
+#             n = int(input(msg) or valor_default)
+#             if not minimo <= n <= maximo:
+#                 raise ValueError(msg_err)
+#         except ValueError as e:
+#             print(e)
+#         else:
+#             break
+#     return n
+
+
+def input_float(msg, minimo, maximo, valor_default, msg_err):
+    while True:
+        try:
+            x = float(input(msg) or valor_default)
+            if not minimo <= x <= maximo:
+                raise ValueError(msg_err)
+        except ValueError as e:
+            print(e)
+        else:
+            break
+    return x
+
+
 def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma
     assinatura a ser comparada com os textos fornecidos'''
-    print("Bem-vindo ao detector automático de COH-PIAH.")
-    print("Informe a assinatura típica de um aluno infectado:")
+    print("\nBem-vindo ao detector automático de COH-PIAH.")
+    print("Informe a assinatura típica de um aluno infectado:\n")
 
-    wal = float(input("Entre o tamanho médio de palavra:"))
-    ttr = float(input("Entre a relação Type-Token:"))
-    hlr = float(input("Entre a Razão Hapax Legomana:"))
-    sal = float(input("Entre o tamanho médio de sentença:"))
-    sac = float(input("Entre a complexidade média da sentença:"))
-    pal = float(input("Entre o tamanho medio de frase:"))
+    wal = 4.51
+    ttr = 0.693
+    hlr = 0.55
+    sal = 70.82
+    sac = 1.82
+    pal = 38.5
 
+    # wal = float(input_float(
+    #     "Entre o tamanho médio de palavra: ", 0.0, 100.0, wal, ""))
+    # ttr = float(input_float(
+    #     "Entre a relação Type-Token: ", 0.0, 100.0, ttr, ""))
+    # hlr = float(input_float(
+    #     "Entre a Razão Hapax Legomana: ", 0.0, 100.0, hlr, ""))
+    # sal = float(input_float(
+    #     "Entre o tamanho médio de sentença: ", 0.0, 100.0, sal, ""))
+    # sac = float(input_float(
+    #     "Entre a complexidade média da sentença: ", 0.0, 100.0, sac, ""))
+    # pal = float(input_float(
+    #     "Entre o tamanho medio de frase: ", 0.0, 100.0, pal, ""))
+    print()
     return [wal, ttr, hlr, sal, sac, pal]
 
 
@@ -85,16 +124,45 @@ def n_palavras_diferentes(lista_palavras):
     return len(freq)
 
 
-# def compara_assinatura(as_a, as_b):
-#     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve
-#     devolver o grau de similaridade nas assinaturas.'''
-#     pass
+def tam_medio_palavra(lista_palavras, n_palavras):
+
+    # print(lista_palavras)
+
+    i = 0
+    wal = 0.0
+    while i < n_palavras:
+        wal += len(lista_palavras[i])
+        i += 1
+    wal /= i
+    return wal
 
 
-# def calcula_assinatura(texto):
-#     '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a
-#     assinatura do texto.'''
-#     pass
+def rel_type_token(lista_palavras, n_palavras):
+
+    ttr = n_palavras_diferentes(lista_palavras) / n_palavras
+    return ttr
+
+    # def compara_assinatura(as_a, as_b):
+    #     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve
+    #     devolver o grau de similaridade nas assinaturas.'''
+    #     pass
+
+
+def rel_hapax_legomana(lista_palavras, n_palavras):
+    hlr = n_palavras_diferentes(lista_palavras) / n_palavras
+    return hlr
+
+
+def calcula_assinatura(texto):
+    '''Essa funcao recebe um texto e deve devolver a
+    assinatura do texto.'''
+    lista_palavras = separa_palavras(texto)
+    n_palavras = len(lista_palavras)
+
+    wal = tam_medio_palavra(lista_palavras, n_palavras)
+    ttr = rel_type_token(lista_palavras, n_palavras)
+    hlr = rel_hapax_legomana(lista_palavras, n_palavras)
+    print(wal, ttr, hlr)
 
 
 # def avalia_textos(textos, ass_cp):
@@ -102,3 +170,24 @@ def n_palavras_diferentes(lista_palavras):
 #     ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade
 #     de ter sido infectado por COH-PIAH.'''
 #     pass
+
+
+def remove_pontuacao(texto):
+    return re.sub(r'[^\w\s]', '', texto)
+
+
+def main():
+    # texto = "Então resolveu ir brincar com a Máquina pra ser também " + \
+    #     "imperador dos filhos da mandioca. Mas as três cunhas deram " + \
+    #     "muitas risadas e falaram que isso de deuses era gorda mentira " + \
+    #     "antiga, que não tinha deus não e que com a máquina ninguém " + \
+    #     "não brinca porque ela mata. A máquina não era deus não, nem " + \
+    #     "possuía os distintivos femininos de que o herói gostava tanto."
+    texto = "Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova."
+    # le assinatura a ser comparada com os textos fornecidos
+    le_assinatura()
+    # recebe um texto e devolve a assinatura do texto
+    calcula_assinatura(remove_pontuacao(texto))
+
+
+main()
